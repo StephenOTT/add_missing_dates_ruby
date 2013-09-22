66fdadb984a8e135ec4b3c1ea6e03a538ed27a06
Stephen Russett <stephen.russett@ottawa.ca>
2013-09-21 20:31:06 -0400

    linked methods together

--- add_missing_dates_months.rb
+++ add_missing_dates_months.rb
@@ -3,9 +3,8 @@ require 'active_support/core_ext'
 
 class DateManipulate
 	
-	def initialize
-		# @creationDateCount = [{"_id"=>{"year"=>2012, "month"=>10}, "number"=>53}, {"_id"=>{"year"=>2012, "month"=>12}, "number"=>58}, {"_id"=>{"year"=>2011, "month"=>8}, "number"=>8}, {"_id"=>{"year"=>2012, "month"=>11}, "number"=>75}, {"_id"=>{"year"=>2011, "month"=>9}, "number"=>58}, {"_id"=>{"year"=>2013, "month"=>9}, "number"=>67}, {"_id"=>{"year"=>2013, "month"=>6}, "number"=>561}, {"_id"=>{"year"=>2012, "month"=>9}, "number"=>102}, {"_id"=>{"year"=>2013, "month"=>5}, "number"=>609}, {"_id"=>{"year"=>2012, "month"=>7}, "number"=>84}, {"_id"=>{"year"=>2010, "month"=>12}, "number"=>1}, {"_id"=>{"year"=>2013, "month"=>7}, "number"=>595}, {"_id"=>{"year"=>2013, "month"=>3}, "number"=>253}, {"_id"=>{"year"=>2012, "month"=>6}, "number"=>129}, {"_id"=>{"year"=>2012, "month"=>2}, "number"=>95}, {"_id"=>{"year"=>2012, "month"=>4}, "number"=>50}, {"_id"=>{"year"=>2013, "month"=>8}, "number"=>736}, {"_id"=>{"year"=>2013, "month"=>1}, "number"=>242}, {"_id"=>{"year"=>2011, "month"=>11}, "number"=>46}, {"_id"=>{"year"=>2012, "month"=>3}, "number"=>68}, {"_id"=>{"year"=>2011, "month"=>10}, "number"=>48}, {"_id"=>{"year"=>2012, "month"=>1}, "number"=>70}, {"_id"=>{"year"=>2012, "month"=>8}, "number"=>70}, {"_id"=>{"year"=>2013, "month"=>2}, "number"=>266}, {"_id"=>{"year"=>2011, "month"=>12}, "number"=>47}, {"_id"=>{"year"=>2013, "month"=>4}, "number"=>449}, {"_id"=>{"year"=>2012, "month"=>5}, "number"=>77}]
-		# @creationDateCount = [{"_id"=>{"year"=>2012, "month"=>01}, "number"=>58},{"_id"=>{"year"=>2012, "month"=>9}, "number"=>53}, {"_id"=>{"year"=>2012, "month"=>12}, "number"=>58}]
+	def initialize (hash)
+		self.sortHash(hash)
 	end
 
 	def sortHash (hash)
@@ -23,19 +22,8 @@ class DateManipulate
 			sortedHash[y] = dateFormatChangeHash[y]
 		end
 
-		return sortedHash
-	end
-
-	def simpleHashSort (hash)
-		sortedDates = hash.keys.sort
-		sortedHash = {}
-
-		sortedDates.each do |y|
-			sortedHash[y] = hash[y]
-		end
-
-		puts "sorted Hash:  #{pp sortedHash}"
-		return sortedHash
+		self.addMissingMonths(sortedHash)
+		# return sortedHash
 	end
 
 	def addMissingMonths (datesHash)
@@ -51,8 +39,12 @@ class DateManipulate
 
 			count += 1
 		end
+		mergedHash = result.merge!(datesHash)
+		self.simpleHashSort(mergedHash)
 
-		return result.merge!(datesHash)
+		# return result.merge!(datesHash)
+		puts sortedHash
+		return sortedHash
 	end
 end
 
